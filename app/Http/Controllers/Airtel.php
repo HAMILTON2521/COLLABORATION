837c30c1b71e0d0aea882e6053c8183325017ea7
Bryson Mahuvi <bmahuvi@gmail.com>
2025-01-23 01:42:41 +0300

    Update

--- app/Http/Controllers/Airtel.php
+++ app/Http/Controllers/Airtel.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class Airtel extends Controller
 {
-    public function callback()
+    public function callback(Request $request)
     {
+        Log::info(json_encode($request->all()));
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Callback received successfully'
