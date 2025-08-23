1264c7ce88957ba9e201b5cc529f6b34d22898c6
Bryson Mahuvi <bmahuvi@gmail.com>
2025-08-23 17:38:48 +0300

    Updates

--- resources/views/livewire/equipment/valve-status.blade.php
+++ resources/views/livewire/equipment/valve-status.blade.php
@@ -8,9 +8,17 @@
         </div>
         <div class="col-12 mb-7">
             <p class="mb-1 fs-2">Valve Status</p>
-            <h6 class="fw-semibold mb-0">
-                {{ strtoupper($response['data']['valveStatus']) }}
-            </h6>
+            <div class="d-flex align-items-center justify-content-between">
+                <h6 class="fw-semibold mb-0">
+                    {{ ucfirst($response['data']['valveStatus']) }}
+                </h6>
+                @if ($response['data']['valveStatus'] === 'open')
+                    <iconify-icon icon="tabler:lock-open-2" width="24" height="24"
+                                  class="text-success"></iconify-icon>
+                @else
+                    <iconify-icon icon="tabler:lock" width="24" height="24" class="text-danger"></iconify-icon>
+                @endif
+            </div>
         </div>
     </div>
 </div>
